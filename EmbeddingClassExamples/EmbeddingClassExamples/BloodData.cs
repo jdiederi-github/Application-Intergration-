44868552034de3e9c04d86a6eb89976f802c126d
Jeremy C Diederich <57365755+jdiederi-github@users.noreply.github.com>
2021-05-12 11:16:56 -0500

    Add files via upload
    Embedding Classes examples

---
+++ EmbeddingClassExamples/EmbeddingClassExamples/BloodData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbeddingClassExamples
+{
+    class BloodData
+    {
+        private string bloodType;
+        private string rhFactor;
+
+        //constructor
+        public BloodData()
+        {
+            bloodType = "O";
+            rhFactor = "+";
+        }
+        public BloodData(string btype, string rh)
+        {
+            bloodType = btype;
+            rhFactor = rh;
+        }
+
+        public string BloodType
+        {
+            get
+            {
+                return bloodType;
+            }
+            set
+            {
+                bloodType = value;
+            }
+        }
+
+        public string RhFactor
+        {
+            get
+            {
+                return rhFactor; 
+            }
+            set
+            {
+                rhFactor = value;
+            }
+        }
+
+    }
+}
---
+++ EmbeddingClassExamples/EmbeddingClassExamples/Demographics.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbeddingClassExamples
+{
+    class Demographics
+    {
+       
+
+        private string email;
+        private string nextOfKin;
+        private string phoneNumber;
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string NextOfKin
+        {
+            get
+            {
+                return nextOfKin;
+            }
+            set
+            {
+                nextOfKin = value;
+            }
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+
+        public Demographics(string email, string phoneNumber, string nextOfKin)
+        {
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.nextOfKin = nextOfKin;
+        }
+
+    }
+}
